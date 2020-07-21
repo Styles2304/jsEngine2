413190f637acc223a99b5bec94d99685710c9d9e
Styles2304 <styles2304@gmail.com>
2020-07-21 06:34:05 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 "use strict"
 
 // Create Game
-    const Game = new GAME("gameCanvas", "gameContainer", 800, 350, 30, true);
+    const Game = new GAME("gameCanvas", "gameContainer", 400, 200, 30, true);
 
 // States
     Game.addState("Boot", ["start", "init"], ["update", "draw"]);
