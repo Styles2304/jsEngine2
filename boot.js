7abecf6bbdc8eb2e7b572d1f913f06756d264a98
Styles2304 <styles2304@gmail.com>
2020-07-22 15:06:02 -0400

    Update boot.js

--- boot.js
+++ boot.js
@@ -5,20 +5,19 @@ Boot.start = function() { if (Game.debug) { console.log("Boot State"); } }
 Boot.wind = null;
 
 Boot.init = function() {
-    for (var a = 0; a < 10; a++) {
-        var _mass = Math.floor(Math.random() * (3-1) + 1);
-        this.curWorld.addEnt(
-            "cube" + a,
-            Math.floor(Math.random() * this.curWorld.width),
-            5,
-            _mass * 20, _mass * 20);
+    for (var a = 0; a < 10; a++) { // Create 10 basic ents for physics testing
+        var _mass = Math.floor(Math.random() * (200-1) + 1);
 
-        var _cube = this.curWorld.ents["cube" + a];
+        var _ent = new Ent(
+            this.game,
+            this.curWorld,
+            Math.floor(Math.random() * this.curWorld.width), 50,
+            _mass, // width
+            _mass, // height
+            _mass);
 
-        _cube.physics.collideWithWorld = true;
-
-        _cube.draw = function() {
-            this.ctx.fillStyle = "rgba(255,255,0,.5)";
+        _ent.draw = function() {
+            this.ctx.fillStyle = "rgba(255,255,0,0.5)";
             this.ctx.fillRect(this.pos.x, this.pos.y, this.width, this.height);
         }
     }
@@ -26,14 +25,8 @@ Boot.init = function() {
     this.curWorld.enablePhysics();
 
     this.wind = new Vect(Math.random() * (1+1) - 1, 0);
-    console.log(this.wind.x);
 }
 
-Boot.update = function() {
-    for (var a = 0; a < 10; a++) {
-        let _cube = this.curWorld.ents["cube" + a];
-        _cube.applyForce(this.wind);
-    }
-}
+Boot.update = function() {}
 
 Boot.draw = function() {}
