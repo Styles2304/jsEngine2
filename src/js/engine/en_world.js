ebb7eeb5e6c7a6758ebd70202a5e455ce6cc637f
Styles2304 <styles2304@gmail.com>
2020-07-22 06:39:59 -0400

    Update en_world.js

--- src/js/engine/en_world.js
+++ src/js/engine/en_world.js
@@ -17,6 +17,15 @@ class World {
             new Vect(this.width, this.height),
             new Vect(0, this.height)
         );
+        this.physics = {
+            enabled: false,
+            gravity: new Vect(0, 3),
+            friction: new Vect(),
+            fricStr: 1,
+            drag: new Vect(),
+            dragStr: 0.00005,
+            bounce: .75
+        };
     }
 
     debugDraw() {
@@ -27,5 +36,13 @@ class World {
 
     addEnt(name, x, y, width, height, physics, mass) {
         this.ents[name] = new Ent(this.g, this, x, y, width || 0, height || 0, physics || false, mass || 1);
+    }
+
+    enablePhysics() {
+        this.physics.enabled = true;
+
+        Object.keys(this.ents).forEach(key => {
+            this.ents[key].enablePhysics();
+        });
     }
 }
