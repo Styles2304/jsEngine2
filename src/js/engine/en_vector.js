5154a09e2d4b0246bed954a2d642af9480f36475
Styles2304 <styles2304@gmail.com>
2020-07-27 06:56:10 -0400

    Update en_vector.js

--- src/js/engine/en_vector.js
+++ src/js/engine/en_vector.js
@@ -1,91 +1,95 @@
 "use strict";
-//=======================================================//
-// Vector Class
-//=======================================================//
+/**
+ * Vector Class
+ */
+    class Vect {
+        /**
+         * Custom Vector class
+         * @param {number} x 
+         * @param {number} y 
+         */
+        constructor(x, y) {
+            this.x = x || 0;
+            this.y = y || 0;
+        }
 
-class Vect {
-    constructor(x, y) {
-        this.x = x || 0;
-        this.y = y || 0;
-    }
+        add(v) {
+            this.x += v.x;
+            this.y += v.y;
+        }
 
-    add(v) {
-        this.x += v.x;
-        this.y += v.y;
-    }
+        static add(v1, v2) {
+            var _v = new Vect();
+            _v.x = v1.x + v2.x;
+            _v.y = v1.y + v2.y;
+            return _v;
+        }
 
-    static add(v1, v2) {
-        var _v = new Vect();
-        _v.x = v1.x + v2.x;
-        _v.y = v1.y + v2.y;
-        return _v;
-    }
+        sub(v) {
+            this.x -= v.x;
+            this.y -= v.y;
+        }
 
-    sub(v) {
-        this.x -= v.x;
-        this.y -= v.y;
-    }
+        static sub(v1, v2) {
+            var _v = new Vect();
+            _v.x = v1.x - v2.x;
+            _v.y = v1.y - v2.y;
+            return _v;
+        }
 
-    static sub(v1, v2) {
-        var _v = new Vect();
-        _v.x = v1.x - v2.x;
-        _v.y = v1.y - v2.y;
-        return _v;
-    }
+        mult(scal) {
+            this.x = this.x * scal;
+            this.y = this.y * scal;
+        }
 
-    mult(scal) {
-        this.x = this.x * scal;
-        this.y = this.y * scal;
-    }
+        static mult(v, scal) {
+            var _v = new Vect();
+            _v.x = v.x * scal;
+            _v.y = v.y * scal;
+            return _v;
+        }
 
-    static mult(v, scal) {
-        var _v = new Vect();
-        _v.x = v.x * scal;
-        _v.y = v.y * scal;
-        return _v;
-    }
+        div(scal) {
+            this.x = this.x / scal;
+            this.y = this.y / scal;
+        }
 
-    div(scal) {
-        this.x = this.x / scal;
-        this.y = this.y / scal;
-    }
+        static div(v, scal) {
+            var _v = new Vect();
+            _v.x = v.x / scal;
+            _v.y = v.y / scal;
+            return _v;
+        }
 
-    static div(v, scal) {
-        var _v = new Vect();
-        _v.x = v.x / scal;
-        _v.y = v.y / scal;
-        return _v;
-    }
+        norm() {
+            this.x = this.x / this.getMag();
+            this.y = this.y / this.getMag();
+        }
 
-    norm() {
-        this.x = this.x / this.getMag();
-        this.y = this.y / this.getMag();
-    }
+        getMag() {
+            return Math.sqrt((this.x * this.x) + (this.y * this.y));
+        }
 
-    getMag() {
-        return Math.sqrt((this.x * this.x) + (this.y * this.y));
-    }
+        magSq() {
+            return (this.x * this.x + this.y * this.y);
+        }
 
-    magSq() {
-        return (this.x * this.x + this.y * this.y);
-    }
+        setMag(mag) {
+            this.norm();
+            this.mult(mag);
+        }
 
-    setMag(mag) {
-        this.norm();
-        this.mult(mag);
-    }
+        limit(max) {
+            if (this.magSq() > max*max) {
+                this.setMag(max);
+            }
+        }
 
-    limit(max) {
-        if (this.magSq() > max*max) {
-            this.setMag(max);
+        copy() {
+            return new Vect(this.x, this.y);
+        }
+        
+        static copy(v) {
+            return new Vect(v.x, v.y);
         }
     }
-
-    copy() {
-        return new Vect(this.x, this.y);
-    }
-    
-    static copy(v) {
-        return new Vect(v.x, v.y);
-    }
-}
