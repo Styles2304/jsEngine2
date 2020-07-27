c355aba11b62ea21676cb7150e4019258f1a3b05
Styles2304 <styles2304@gmail.com>
2020-07-27 06:56:07 -0400

    Update en_world.js

--- src/js/engine/en_world.js
+++ src/js/engine/en_world.js
@@ -1,43 +1,90 @@
 "use strict";
-//=======================================================//
-// World Class
-//=======================================================//
+/** World Class */
+    class World {
+        /**
+         * The World class contained by a State. Contains the camera, all entities, and
+         * Cells
+         * @param {{}} Game Reference to the master GAME object
+         * @param {Number} width Width of the World
+         * @param {Number} height Height of the World
+         */
+        constructor(Game, width, height) {
+            this.Game = Game;
+            this.ctx = Game.CONTEXT;
+            this.width = width;
+            this.height = height;
+            this.cells = [];
+            this.ents = [];
+            this.collision = Array(
+                new Vect(0, 0),
+                new Vect(this.width, 0),
+                new Vect(this.width, this.height),
+                new Vect(0, this.height)
+            );
+            this.physics = {
+                enabled: false,
+                gravity: new Vect(0, 3),
+                friction: 1,
+                drag: 0.0001,
+                bounce: 0.75
+            };
+            this.camera = {
+                follow: null,
+                pos: new Vect(),
+                width: this.Game.STAGE.width,
+                height: this.Game.STAGE.height,
+                dzX: this.width / 2,
+                dzY: this.height / 2
+            }
+        }
 
-class World {
-    constructor(game, width, height) {
-        this.game = game;
-        this.ctx = game.CONTEXT;
-        this.width = width;
-        this.height = height;
-        this.cells = [];
-        this.ents = [];
-        this.collision = Array(
-            new Vect(0, 0),
-            new Vect(this.width, 0),
-            new Vect(this.width, this.height),
-            new Vect(0, this.height)
-        );
-        this.physics = {
-            enabled: false,
-            gravity: new Vect(0, 3),
-            friction: new Vect(),
-            fricStr: 1,
-            drag: new Vect(),
-            dragStr: 0.0001,
-            bounce: .75
-        };
-    }
+        update() {
+            var _c = this.camera;
+            _c.pos.x = (_c.follow.pos.x - _c.width / 2);
+            _c.pos.y = (_c.follow.pos.y - _c.height /2);
 
-    debugDraw() {
-        this.ctx.strokeStyle = "rgb(255,0,0)";
-        this.ctx.lineWidth = 5;
-        this.ctx.strokeRect(0, 0, this.width, this.height);
-    }
+            if (_c.follow.pos.y > this.height - _c.dzY) { _c.pos.y = this.height - _c.height; }
+            if (_c.follow.pos.y < _c.dzY) { _c.pos.y = 0; }
+            if (_c.follow.pos.x > this.width - _c.dzX) { _c.pos.x = this.width - _c.width; }
+            if (_c.follow.pos.x < _c.dzX) { _c.pos.x = 0; }
 
-    enablePhysics() {
-        this.physics.enabled = true;
-        this.ents.forEach(function(ent) {
-            ent.enablePhysics();
-        });
+            this.ctx.setTransform(1,0,0,1, -_c.pos.x, -_c.pos.y);
+        }
+
+        debugDraw() {
+            // World
+            this.ctx.strokeStyle = "rgb(255,0,0)"; // Red
+            this.ctx.lineWidth = 3;
+            this.ctx.strokeRect(1, 1, this.width - 2, this.height - 2);
+
+            // Camera
+            this.ctx.strokeStyle = "rgb(0,0,255)"; // Blue
+            this.ctx.lineWidth = 2;
+            this.ctx.strokeRect(
+                this.camera.pos.x + 4,
+                this.camera.pos.y + 4,
+                this.camera.width - 9,
+                this.camera.height - 8
+            );
+            this.ctx.lineWidth = 1;
+        }
+
+        /**
+         * Enables physics on the Specified World and all of the Entities it contains
+         * @param {Object.<Vect>} gravity 
+         * @param {number} friction 
+         * @param {number} drag 
+         * @param {number} bounce 
+         */
+        enablePhysics(gravity, friction, drag, bounce) {
+            this.physics.gravity = gravity || new Vect(0,3);
+            this.physics.friction = friction || 1;
+            this.physics.drag = drag || 0.0001;
+            this.physics.bounce = bounce || 0.75;
+
+            this.physics.enabled = true;
+            this.ents.forEach(function(ent) {
+                ent.enablePhysics();
+            });
+        }
     }
-}
