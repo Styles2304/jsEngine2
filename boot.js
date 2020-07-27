81429436b907af66040ab94fda593e4265a259c0
Styles2304 <styles2304@gmail.com>
2020-07-27 06:56:23 -0400

    Update boot.js

--- boot.js
+++ boot.js
@@ -6,30 +6,20 @@ Boot.start = function() { if (Game.debug) { console.log("Boot State"); } }
 Boot.init = function() {
     this.curWorld.enablePhysics();
     
-    // var _wind = new Vect(5,0);
+    var _player = new Ent(
+        this.Game,
+        this.curWorld,
+        this.Game.STAGE.width / 2, 50,
+        50,
+        50,
+        50);
 
-    var _ents = 1;
-    
-    for (var a = 0; a < _ents; a++) { // Create 10 basic ents for physics testing
-        var _mass = Math.floor(Math.random() * (200-1) + 1);
+    this.curWorld.camera.follow = _player;
 
-        var _ent = new Ent(
-            this.game,
-            this.curWorld,
-            Math.floor(Math.random() * this.curWorld.width), 50,
-            _mass, // width
-            _mass, // height
-            _mass);
-
-        _ent.draw = function() {
-            this.ctx.fillStyle = "rgba(255,255,0,0.5)";
-            this.ctx.fillRect(this.relativePos.x, this.relativePos.y, this.width, this.height);
-        }
-
-        _ent.offset = _ent.center.copy();
-
-        _ent.rotate(45);
+    _player.init = function() {
+        this.offset = _player.center.copy();
     }
+    _player.update = function() {}
 }
 
 Boot.update = function() {}
