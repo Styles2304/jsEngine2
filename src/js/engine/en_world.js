54ea3f780ed2a062b407e50433b6884ba7dc3fbd
Styles2304 <styles2304@gmail.com>
2020-07-28 05:38:16 -0400

    Update en_world.js

--- src/js/engine/en_world.js
+++ src/js/engine/en_world.js
@@ -1,10 +1,13 @@
 "use strict";
-/** World Class */
+/**
+ * World Class
+ * @typedef {{}} World
+ */
     class World {
         /**
-         * The World class contained by a State. Contains the camera, all entities, and
-         * Cells
-         * @param {Game} Game Reference to the master GAME object
+         * A World is a self contained object inside the State. Contains the camera, all Entities, and
+         * Cells. Equivalent of different "levels"
+         * @param {GAME} Game Reference to the master GAME object
          * @param {Number} width Width of the World
          * @param {Number} height Height of the World
          */
@@ -16,21 +19,21 @@
             this.cells = [];
             this.ents = [];
             this.collision = Array(
-                new Vect(0, 0),
-                new Vect(this.width, 0),
-                new Vect(this.width, this.height),
-                new Vect(0, this.height)
+                new Vector(0, 0),
+                new Vector(this.width, 0),
+                new Vector(this.width, this.height),
+                new Vector(0, this.height)
             );
             this.physics = {
                 enabled: false,
-                gravity: new Vect(0, 3),
+                gravity: new Vector(0, 3),
                 friction: 1,
                 drag: 0.0001,
                 bounce: 0.75
             };
             this.camera = {
                 follow: null,
-                pos: new Vect(),
+                pos: new Vector(),
                 width: this.Game.STAGE.width,
                 height: this.Game.STAGE.height,
                 dzX: this.width / 2,
@@ -38,6 +41,10 @@
             }
         }
 
+        /**
+         * Runs calculations on the World and all of it's children Elements
+         * @method update
+         */
         update() {
             var _c = this.camera;
             _c.pos.x = (_c.follow.pos.x - _c.width / 2);
@@ -51,6 +58,10 @@
             this.ctx.setTransform(1,0,0,1, -_c.pos.x, -_c.pos.y);
         }
 
+        /**
+         * If (GAME.debug) this method draws World and Camera bounds to the canvas
+         * @method debugDraw
+         */
         debugDraw() {
             // World
             this.ctx.strokeStyle = "rgb(255,0,0)"; // Red
@@ -70,14 +81,14 @@
         }
 
         /**
-         * Enables physics on the Specified World and all of the Entities it contains
-         * @param {Object.<Vect>} gravity 
+         * Enables physics on the Specified World and all of it's children Elements
+         * @param {Vector} gravity 
          * @param {number} friction 
          * @param {number} drag 
          * @param {number} bounce 
          */
         enablePhysics(gravity, friction, drag, bounce) {
-            this.physics.gravity = gravity || new Vect(0,3);
+            this.physics.gravity = gravity || new Vector(0,3);
             this.physics.friction = friction || 1;
             this.physics.drag = drag || 0.0001;
             this.physics.bounce = bounce || 0.75;
