863b401eebcae4fdd0111104e093ac7bfb250bfe
Styles2304 <styles2304@gmail.com>
2020-07-29 06:45:43 -0400

    Update boot.js

--- boot.js
+++ boot.js
@@ -4,16 +4,15 @@ let Boot = Game.states.Boot;
 Boot.start = function() { if (Game.debug) { console.log("Boot State"); } }
 
 Boot.init = function() {
-    this.curWorld.addCells(200,200);
     this.curWorld.enablePhysics();
 
     var _player = new Entity(
         this.Game,
         this.curWorld,
-        this.Game.STAGE.width / 2, 50,
+        this.curWorld.width / 2, 50,
         50,
         50,
-        50);
+        10);
 
     this.curWorld.camera.follow = _player;
 
