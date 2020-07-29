1cd7c6c47fee8b5adca4e0ec91b3f1745b88802c
Styles2304 <styles2304@gmail.com>
2020-07-29 15:48:37 -0400

    Update en_cell.js

--- src/js/engine/en_cell.js
+++ src/js/engine/en_cell.js
@@ -19,10 +19,10 @@
             this.Game = Game;
             this.ctx = Game.CONTEXT;
             this.World = World;
-            this.x = x || 0;
-            this.y = y || 0;
+            this.pos = new Vector(x || 0, y || 0);
             this.width = width || Game.STAGE.width;
             this.height = height || Game.STAGE.height;
+            this.ents = [];
         }
 
         /**
@@ -37,32 +37,32 @@
                 _hMod = 0;
             
             if (this.World.cells.length == 1) {
-                this.ctx.strokeRect(this.x + 1, this.y + 1, this.width - 2, this.height - 2);
+                this.ctx.strokeRect(this.pos.x + 1, this.pos.y + 1, this.width - 2, this.height - 2);
             } else {
                 this.ctx.beginPath();
 
-                if (this.x == this.World.width - this.width) {
+                if (this.pos.x == this.World.width - this.width) {
                     _wMod = -1;
                 }
 
-                if (this.y == this.World.height - this.height) {
+                if (this.pos.y == this.World.height - this.height) {
                     _hMod = -1;
                 }
 
-                if (this.x == 0) {
-                    this.ctx.moveTo(this.x + 1, this.y + 1);
-                    this.ctx.lineTo(this.x + 1, this.y + this.height + _hMod);
-                    this.ctx.moveTo(this.x + 1, this.y + 1);
+                if (this.pos.x == 0) {
+                    this.ctx.moveTo(this.pos.x + 1, this.pos.y + 1);
+                    this.ctx.lineTo(this.pos.x + 1, this.pos.y + this.height + _hMod);
+                    this.ctx.moveTo(this.pos.x + 1, this.pos.y + 1);
                 }
 
-                if (this.y == 0) {
-                    this.ctx.moveTo(this.x + 1, this.y + 1);
-                    this.ctx.lineTo(this.x + this.width + _wMod, this.y + 1);
+                if (this.pos.y == 0) {
+                    this.ctx.moveTo(this.pos.x + 1, this.pos.y + 1);
+                    this.ctx.lineTo(this.pos.x + this.width + _wMod, this.pos.y + 1);
                 }
                 
-                this.ctx.moveTo(this.x + this.width + _wMod, this.y);
-                this.ctx.lineTo(this.x + this.width + _wMod, this.y + this.height + _hMod);
-                this.ctx.lineTo(this.x, this.y + this.height + _hMod);
+                this.ctx.moveTo(this.pos.x + this.width + _wMod, this.pos.y);
+                this.ctx.lineTo(this.pos.x + this.width + _wMod, this.pos.y + this.height + _hMod);
+                this.ctx.lineTo(this.pos.x, this.pos.y + this.height + _hMod);
                 this.ctx.stroke();
             }
             this.ctx.setLineDash([]);
