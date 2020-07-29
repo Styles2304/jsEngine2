56e59631653e602511af480f39a2a13ad3fb5cba
Styles2304 <styles2304@gmail.com>
2020-07-29 15:47:55 -0400

    Update boot.js

--- boot.js
+++ boot.js
@@ -4,6 +4,7 @@ let Boot = Game.states.Boot;
 Boot.start = function() { if (Game.debug) { console.log("Boot State"); } }
 
 Boot.init = function() {
+    this.curWorld.setWorld(2000, 400);
     this.curWorld.enablePhysics();
 
     var _player = new Entity(
@@ -19,7 +20,21 @@ Boot.init = function() {
     _player.init = function() {
         this.offset = _player.center.copy();
     }
-    _player.update = function() {}
+
+    _player.update = function() {
+        if (this.Game.keys.left) { this.pos.x -= 10; }
+        if (this.Game.keys.right) { this.pos.x += 10; }
+        if (this.Game.keys.space) { this.assignCell(); }
+    }
+
+    var _testEnt = new Entity(
+        this.Game,
+        this.curWorld,
+        100, 50,
+        50, 50, 10
+    );
+
+    _testEnt.render = false;
 }
 
 Boot.update = function() {}
