03e4e6e9420c4bd3a43c2a4eb01680870245c501
Styles2304 <styles2304@gmail.com>
2020-08-07 06:35:51 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,8 @@
     const Game = new GAME("gameContainer", 400, 400, 30, true);
 
 // States
-    Game.addState("Boot", ["start", "init"], ["update", "draw"]);
+    Game.addState("Boot", ["start", "init"], ["draw"]);
+    Game.addState("Play", ["start", "init"], ["update", "draw"]);
 
 // Start State
     Game.startState("Boot");
