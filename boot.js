c088931dcb1563148a1327e017314d3b12dff8ce
Styles2304 <styles2304@gmail.com>
2020-08-11 06:39:03 -0400

    Update boot.js

--- boot.js
+++ boot.js
@@ -3,15 +3,24 @@ let Boot = Game.states.Boot;
 Boot.start = function() { if (Game.debug) { console.log("Boot State"); } }
 
 Boot.init = function() {
+    this.loaded = false;
     Game.loadImages([
-        "src/img/spritesheet.png"
-    ], Game.startState("Play"));
+        ["src/img/spritesheet.png", "spriteSheet", 276, 281],
+        ["src/img/tilesheet.png", "tileSheet", 48, 16]
+    ], this.loaded = true);
     this.loadingText = "Loading...";
+    this.continueText = "Press Spacebar";
 }
 
 Boot.draw = function() {
     this.ctx.font = "30px Arial";
     this.ctx.fillStyle = "#FFF";
-    this.ctx.fillText(this.loadingText, Game.STAGE.center.x - (this.ctx.measureText(this.loadingText).width / 2), Game.STAGE.center.y);
 
+    if (this.loaded) {
+        this.ctx.fillText(this.continueText, Game.STAGE.center.x - (this.ctx.measureText(this.continueText).width / 2), Game.STAGE.center.y);
+        // if (Game.keys.space) { Game.startState("Play"); }
+        Game.startState("Play");
+    } else {
+        this.ctx.fillText(this.loadingText, Game.STAGE.center.x - (this.ctx.measureText(this.loadingText).width / 2), Game.STAGE.center.y);
+    }
 }
